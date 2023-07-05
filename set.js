4e598509d8bb5ec0abc0e87474b61da3fd469a35
ga8226 <ga8226@naver.com>
2023-07-05 11:23:13 +0900

    후치연산으로 월 부터 나오게 하는법

--- set.js
+++ set.js
@@ -2,9 +2,14 @@ let count = ["월","화","수","목","금","토","일"];
 let day = 0;
 
 const timer = setInterval(()=>{
+ 
+  console.log(count[day])
+  if( day == 5){
+    clearInterval(timer)
+    //토요일까지 출력하고 멈추기
+  }
   day++;
   day%=count.length;
-  console.log(count[day])
 },1000) 
 
 //10초 뒤에 멈추는방법 setTimeout , clearInterval
