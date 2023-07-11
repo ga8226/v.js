afe62517a26f7c1a28ba705f16f92d1b9c163acb
ga8226 <ga8226@naver.com>
2023-07-11 12:07:28 +0900

    7/11일 자바공부

--- 2Arr.js
+++ 2Arr.js
@@ -1,7 +1,30 @@
-const db = [
-    ["화화",3],
-    ["멍란",7],
-    ["푸바오",3]
+const db = {
+    mypet : [
+        ["화화",3],
+        ["멍란",7],
+        ["푸바오",3]
+    ],
+    //푸바오 3살
+    action : function(){
+        console.log(this.mypet[2][0]+"는 "+this.mypet[2][1] +"살입니다.")
+        this.mypet.push(["쌍둥이",0])
+        console.log(this.mypet)
 
-]
-console.log(db[2][0] + "는 "+db[2][1]+"살입니다")
+    }
+}
+
+//독립함수
+ function auction(a , x){
+    db.mypet[1][1]=8; //7을 8으로 수정
+    console.log(a[x][0]+"는 "+a[x][1] +"살입니다.") //a[x][1] = mypet[1][1] 
+    
+    // this.mypet.push(["쌍둥이",0])
+     
+}
+//명란은 8살입니다
+db.action();
+auction(db.mypet,1)
+  
+
+
+
---
+++ my.js
@@ -0,0 +1,13 @@
+const hbs = {
+    list : [
+        ["nct","mark"] ,
+        ["mx" , "형원"] ,
+        ["sv" , "조슈아"]
+    ] ,
+
+    action : function(){
+        console.log
+
+    }
+
+}
